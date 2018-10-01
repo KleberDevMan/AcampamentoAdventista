49f15355b096bcdc989129066af171516190ab97
KleberDevMen <38785907+KleberDevMen@users.noreply.github.com>
2018-09-30 23:28:36 -0300

    ultimo commit

--- app/src/main/java/com/example/kleber/acampamentoadventista/activity/MolduraActivity.java
+++ app/src/main/java/com/example/kleber/acampamentoadventista/activity/MolduraActivity.java
@@ -5,7 +5,7 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 
 import com.example.kleber.acampamentoadventista.R;
 
@@ -19,7 +19,10 @@ public class MolduraActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moldura);
 
-        //        configuraToolbar();
+        foto = findViewById(R.id.foto_escolhida);
+
+        //configura a toolbar
+        configuraToolbar();
 
         //Recupera foto escolhida pelo usuario
         Bundle bundle = getIntent().getExtras();
@@ -31,19 +34,15 @@ public class MolduraActivity extends AppCompatActivity {
     }
 
     private void configuraToolbar() {
-        if (getSupportActionBar() != null) {
-//
-//            Toolbar toolbar = findViewById(R.id.toolbar);
-//            toolbar.setTitle("");
-            getSupportActionBar().setTitle("");
-            //seta btnVoltar
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//            getSupportActionBar().setDisplayShowHomeEnabled(true);
-            getSupportActionBar().setHomeAsUpIndicator(R.drawable.icon_close_branco_24dp);
-        }
-    }
 
-    private void inicializarComponentes() {
-        foto = findViewById(R.id.foto_escolhida);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Molduras");
+        setSupportActionBar( toolbar );
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.icon_close_branco_24dp);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
     }
 }
