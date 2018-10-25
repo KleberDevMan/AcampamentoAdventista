2c87de2e170bc0428ed4af96dd058575f6fe6acf
KleberDevMen <38785907+KleberDevMen@users.noreply.github.com>
2018-10-25 00:36:11 -0300

    cofigurei o FireBase no projeto ( ok )

--- app/src/main/java/com/example/kleber/acampamentoadventista/activity/RoteiroActivity.java
+++ app/src/main/java/com/example/kleber/acampamentoadventista/activity/RoteiroActivity.java
@@ -2,9 +2,11 @@ package com.example.kleber.acampamentoadventista.activity;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.example.kleber.acampamentoadventista.R;
@@ -14,11 +16,19 @@ import com.example.kleber.acampamentoadventista.fragmentos.roteiros.RoteiroSabad
 import com.example.kleber.acampamentoadventista.fragmentos.roteiros.RoteiroSegundaFragment;
 import com.example.kleber.acampamentoadventista.fragmentos.roteiros.RoteiroSextaFragment;
 import com.example.kleber.acampamentoadventista.fragmentos.roteiros.RoteiroTercaFragment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RoteiroActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+
+    //referencia ao banco de dados
+    private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +46,20 @@ public class RoteiroActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        //Busco roteiros
+        DatabaseReference roteiros = referencia.child( "roteiros" );
+        roteiros.addValueEventListener(new ValueEventListener() {
+            //Chamado sempre que algum dado é alterado no banco
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.i("FIREBASE", dataSnapshot.getValue().toString());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         //adiciona fragmentos
         AdaptadorDePaginas adapter = new AdaptadorDePaginas(getSupportFragmentManager());
